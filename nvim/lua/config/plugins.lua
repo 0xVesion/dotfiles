c5516cb2a1526ed2dec707c8cb8ae626ef8a3516
Vincent Engel <ve@vesion.io>
2022-07-17 17:58:21 +0200

    Added config for completions in neovim

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -1,10 +1,24 @@
-vim.cmd [[packadd packer.nvim]]
+vim.cmd('packadd packer.nvim')
 
 local packer = require('packer')
 
 return packer.startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'neovim/nvim-lspconfig'
+  
+  -- Colorscheme
   use 'folke/tokyonight.nvim'
   use 'folke/lsp-colors.nvim'
+  
+  -- Completions
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  
+  -- Snippets
+  use 'L3MON4D3/LuaSnip'
+  use 'saadparwaiz1/cmp_luasnip'
+  use 'rafamadriz/friendly-snippets'
 end)

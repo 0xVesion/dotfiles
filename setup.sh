46b8a22aea7f61a1e19f6fc7b5421d270435f570
Vincent Engel <ve@vesion.io>
2022-07-17 17:12:58 +0200

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,31 @@
+# Update macOS
+softwareupdate --all --install --force
+
+# Install brew if not exists
+if ! command -v brew &> /dev/null
+then
+   echo "Installing brew..."
+   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Install brew apps
+cd ~/.dotfiles/brew
+brew bundle
+brew cleanup
+brew doctor
+
+# Install AppStore apps
+mas install 1037126344 # Apple Configurator
+mas install 497799835 # Xcode
+
+# Install oh my zsh if not exists
+if [[ ! -e ~/.oh-my-zsh ]]; then
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+# Symlink dotfiles
+ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/ssh/config ~/.ssh/config
+ln -s ~/.dotfiles/nvim ~/.config/nvim
+ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
+

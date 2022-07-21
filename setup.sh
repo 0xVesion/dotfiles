0d8226fa01000f7d450ef73ee043abd903c531f7
Vincent Engel <ve@vesion.io>
2022-07-21 20:35:21 +0200

    Added additional apps to setup.sh

--- setup.sh
+++ setup.sh
@@ -17,15 +17,29 @@ brew doctor
 # Install AppStore apps
 mas install 1037126344 # Apple Configurator
 mas install 497799835 # Xcode
+mas install 405399194 # Kindle
+mas install 409183694 # Keynote
+
+# Install ruby gems
+sudo gem install colorls
 
 # Install oh my zsh if not exists
 if [[ ! -e ~/.oh-my-zsh ]]; then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
+# Install z if not exists
+Z_DIR="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z"
+if [[ ! -e $Z_DIR ]]; then
+  git clone https://github.com/agkozak/zsh-z $Z_DIR
+fi
+
+# Install packer if not exists
+if [[ ! -e ~/.local/share/nvim/site/pack/packer/start/packer.nvim ]]; then
+  git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+fi
+
 # Symlink dotfiles
 ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/ssh/config ~/.ssh/config
 ln -s ~/.dotfiles/nvim ~/.config/nvim
-ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
 
